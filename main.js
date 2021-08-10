e48cd189d01cbae98c26e6b5d3a2d674653f04cc
Lucas Nabesima <lucas.nabesima@gmail.com>
2021-08-10 12:49:38 -0300

    Add game logic
    Initially, I implemented the game logic using only switch case, using
    both playerShape and computerPlay functions as arguments, but bugs
    started to appear: When 'paper' was selected by the player, the game
    would register that as 'rock'. This was solved using three ifs and
    nesting switch cases inside them.
    The initial logic is registered and commented for future reference.

--- main.js
+++ main.js
@@ -1,43 +1,94 @@
-const playerShape = window.prompt("Jan Ken Po!");
-console.log(playerShape);
 const shapes = ["Rock", "Paper", "Scissors"];
+const playerShape = function(){
+    let shape = window.prompt("Jan Ken Po!").toLowerCase();
+    console.log(shape);
+    return shape;
+}
 
 const computerPlay = function() {
     const randomComputer = Math.floor(Math.random() * shapes.length);
+    console.log(shapes[randomComputer]);
     return shapes[randomComputer];
 }
 
-function janKen(_player, _computer){
-    switch(playerShape, computerPlay){
-        case "Rock" && "Rock":
-            console.log("Tie!");
-            break;
-        case "Rock" && "Paper":
-            console.log("You lost! Paper beats Rock!");
-            break;
-        case "Rock" && "Scissors":
-            console.log("You win! Rock beats Scissors");
-            break;
-        case "Paper" && "Rock":
-            console.log("You win! Paper beats Rock!");
-            break;
-        case "Paper" && "Paper": 
-            console.log("Tie!");
-            break;
-        case "Paper" && "Scissors":
-            console.log("You lost! Scissors beats Paper!");
-            break;
-        case "Scissors" && "Rock":
-            console.log("You lost! Rock beats Scissors!");
-            break;
-        case "Scissors" && "Paper":
-            console.log("You win! Scissors beats Paper");
-            break;
-        case "Scissors" && "Scissors":
-            console.log("Tie!");
-            break;
+// function janKen(playerShape, computerPlay){
+//     switch(playerShape, computerPlay){
+//         case "rock" && "Rock":
+//             console.log(`It's a tie! Both of you played ${computerPlay.toLowerCase()}.`);
+//             break;
+//         case "rock" && "Paper":
+//             console.log(`You lost! You played ${playerShape}. The computer played ${computerPlay.toLowerCase()}. Paper beats Rock!`);
+//             break;
+//         case "rock" && "Scissors":
+//             console.log(`You win! You played ${playerShape}. The computer played ${computerPlay.toLowerCase()}. Rock beats Scissors!`);
+//             break;
+//         case "paper" && "Rock":
+//             console.log(`You win! You played ${playerShape}. The computer played ${computerPlay.toLowerCase()}. Paper beats Rock!`);
+//             break;
+//         case "paper" && "Paper": 
+//             console.log(`It's a tie! Both of you played ${computerPlay.toLowerCase()}.`);
+//             break;
+//         case "paper" && "Scissors":
+//             console.log(`You lost! You played ${playerShape}. The computer played ${computerPlay.toLowerCase()}. Scissors beats Paper!`);
+//             break;
+//         case "scissors" && "Rock":
+//             console.log(`You lost! You played ${playerShape}. The computer played ${computerPlay.toLowerCase()}. Rock beats Scissors!`);
+//             break;
+//         case "scissors" && "Paper":
+//             console.log(`You win! You played ${playerShape}. The computer played ${computerPlay.toLowerCase()}. Scissors beats Paper!`);
+//             break;
+//         case "scissors" && "Scissors":
+//             console.log(`It's a tie! Both of you played ${computerPlay.toLowerCase()}.`);
+//             break;
+//     }
+// }
+
+
+function janKen(playerShape, computerPlay){
+    if(playerShape == 'rock'){
+        switch(computerPlay){
+            case 'Rock':
+                console.log(`It's a tie! Both of you played ${computerPlay.toLowerCase()}.`);
+                break;
+            case 'Paper':
+                console.log(`You lost! You played ${playerShape} and the computer played ${computerPlay.toLowerCase()}. Paper beats Rock!`);
+                break;
+            case 'Scissors':
+                console.log(`You won! You played ${playerShape} and the computer played ${computerPlay.toLowerCase()}. Rock beats Scissors!`);
+                break;
+        }
+    } else if(playerShape == 'paper'){
+        switch(computerPlay){
+            case 'Rock':
+                console.log(`You won! You played ${playerShape} and the computer played ${computerPlay.toLowerCase()}. Paper beats Rock!`);
+                break;
+            case 'Paper':
+                console.log(`It's a tie! Both of you played ${computerPlay.toLowerCase()}.`);
+                break;
+            case 'Scissors': 
+                console.log(`You lost! You played ${playerShape} and the computer played ${computerPlay.toLowerCase()}. Scissors beats Paper!`);
+                break;
+        }
+    } else if(playerShape == 'scissors'){
+        switch(computerPlay){
+            case 'Rock':
+                console.log(`You lost! You played ${playerShape} and the computer played ${computerPlay.toLowerCase()}. Rock beats Scissors!`);
+                break;
+            case 'Paper':
+                console.log(`You won! You played ${playerShape} and the computer played ${computerPlay.toLowerCase()}. Scissors beats Paper!`);
+                break;
+            case 'Scissors': 
+                console.log(`It's a tie! Both of you played ${computerPlay.toLowerCase()}.`);
+                break;
+        }
     }
 }
 
+function playRound(){
+    alert(`Let's play some Jan Ken Po! Best of 5 rounds.`)
+    for(i = 1; i <= 5; i++){
+        janKen(playerShape(), computerPlay());
+    }
+}
 
-janKen(playerShape, computerPlay());
+playRound();
